f0c3aab6a01017b895db80dda93696d67bc6f151
abdolfarhani <zaarei.net@gmail.com>
2016-12-17 13:08:10 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-define('API_KEY','290793242:AAHIMZv9rEi2o3ZIjhWQoTtwi40LDrkR4oE');
+define('API_KEY','290793242:AAFTgye7WYYmJU9-lRNfOB9OMRgEEouDPxc');
 $admin = "67369633";
 function bot($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
